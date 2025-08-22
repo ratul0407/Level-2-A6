7f44ec4c54510ae38b5f7cef3c63120e3070f325
ratul0407 <rajaulislamratul12@gmail.com>
2025-08-22 18:08:08 +0600

    initiated react router

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import App from "@/App";
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+]);
+export default router;
