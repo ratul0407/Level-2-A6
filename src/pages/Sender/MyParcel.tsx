1dd0814b3c134ceedcf8a2842d331f00922103b0
ratul0407 <rajaulislamratul12@gmail.com>
2025-08-25 13:51:23 +0600

    used cards in my Parcel

--- src/pages/Sender/MyParcel.tsx
+++ src/pages/Sender/MyParcel.tsx
@@ -1,11 +1,39 @@
 import { useGetMyParcelsQuery } from "@/redux/features/parcel/parcel.api";
-
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { format } from "date-fns";
 const MyParcel = () => {
   const { data } = useGetMyParcelsQuery(undefined);
   console.log(data);
   return (
-    <div>
-      <h1>MyParcel</h1>
+    <div className="grid grid-cols-3 gap-4">
+      {data?.data?.data.map((item) => (
+        <Card key={item._id} className="shadow-lg">
+          <CardHeader>
+            <CardTitle>{item?.name}</CardTitle>
+            <CardDescription>
+              {" "}
+              created At: {format(new Date(item?.createdAt), "dd/MM/yy")}
+            </CardDescription>
+            <CardAction>Tk. {item?.cost}</CardAction>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <p>
+                Estimated Delivery date:{" "}
+                {format(new Date(item?.estimatedDeliveryDate), "dd/MM/yy")}
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
     </div>
   );
 };
