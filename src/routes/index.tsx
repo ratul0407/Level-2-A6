960f5bd52bd82df80b35f6d6e32609a1e175a21f
ratul0407 <rajaulislamratul12@gmail.com>
2025-10-12 23:22:37 +0600

    removed faq page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,6 @@ import Contact from "@/pages/Contact";
 import HomePage from "@/pages/HomePage";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
-import Faq from "@/pages/Faq"
 import { generateRoute } from "@/utils/generateRoutes";
 
 import { createBrowserRouter, Navigate } from "react-router";
@@ -35,10 +34,6 @@ const router = createBrowserRouter([
       {
         path: "/contact",
         Component: Contact,
-      },
-      {
-        path: "/faq",
-        Component: Faq,
       },
       {
         path: "*",
